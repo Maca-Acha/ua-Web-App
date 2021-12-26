d520e17c617eb42b71ce80cb9f72837a88302113
lucianhoff <lucianoaguirre007@gmail.com>
2021-12-26 16:49:00 -0300

    favoritos fix

--- backend/controllers/cursosController.js
+++ backend/controllers/cursosController.js
@@ -1,50 +1,54 @@
-const Curso = require('../models/Curso')
+const Curso = require("../models/Curso");
 
 const cursosController = {
-    crearCurso:(req,res)=>{
-        new Curso({...req.body}).save()
-        .then((response)=> res.json({curso: response, success:true}))
-        .catch((e) => res.json({error: e.errors.price, success:false}))
-    },
-    traerCursos: (req,res) => {
-        Curso.find()
-        .populate('tutor')
-        .then((response)=> res.json({response}))
-    },
-    modificarCurso: async(req,res) => {
-        try{
-            actualizado = await Curso.findOneAndUpdate({_id:req.params.id}, {...req.body}, {new:true})
-        }catch(error){
-            console.log(error)
-        }
-        res.json({success:actualizado ? true : false})
-    },
-    borrarCurso: async (req,res) =>{
-        let cursos
-        try{
-            await Curso.findOneAndDelete({_id:req.params.id})
-            cursos = await Curso.find()
-        }catch(error){
-        console.log(error)
-        }
-        res.json({response: cursos, success:true})
-    },
-    favorito: async(req, res) => {
-        const {cursoId, usuarioId, booleano} = req.body
-        try{
-            const curso = await Curso.findOneAndUpdate(
-                {_id:cursoId},
-                booleano
-                ? {$addToSet:{favorito: usuarioId}}
-                : {$pull:{favorito: usuarioId}},
-                {new:true}
-            )
-            res.json({success:true, response: curso, error:null})
-        }catch(e){
-            res.json({success:false, response:null, error:e.message})
-        }
+  crearCurso: (req, res) => {
+    new Curso({ ...req.body })
+      .save()
+      .then((response) => res.json({ curso: response, success: true }))
+      .catch((e) => res.json({ error: e.errors.price, success: false }));
+  },
+  traerCursos: (req, res) => {
+    Curso.find()
+      .populate("tutor")
+      .then((response) => res.json({ response }));
+  },
+  modificarCurso: async (req, res) => {
+    try {
+      actualizado = await Curso.findOneAndUpdate(
+        { _id: req.params.id },
+        { ...req.body },
+        { new: true }
+      );
+    } catch (error) {
+      console.log(error);
     }
+    res.json({ success: actualizado ? true : false });
+  },
+  borrarCurso: async (req, res) => {
+    let cursos;
+    try {
+      await Curso.findOneAndDelete({ _id: req.params.id });
+      cursos = await Curso.find();
+    } catch (error) {
+      console.log(error);
+    }
+    res.json({ response: cursos, success: true });
+  },
+  favorito: async (req, res) => {
+    const { cursoId, usuarioId, booleano } = req.body;
+    try {
+      const cursoFav = await Curso.findOneAndUpdate(
+        { _id: cursoId },
+        booleano
+          ? { $addToSet: { favoritos: usuarioId } }
+          : { $pull: { favoritos: usuarioId } },
+        { new: true }
+      );
+      res.json({ success: true, response: cursoFav, error: null });
+    } catch (e) {
+      res.json({ success: false, response: null, error: e.message });
+    }
+  },
+};
 
-}
-
-module.exports = cursosController 
+module.exports = cursosController;
--- backend/controllers/favoritosController.js
+++ backend/controllers/favoritosController.js
@@ -1,21 +1,21 @@
-const Curso = require('../models/Curso')
+// const Curso = require('../models/Curso')
 
-const favoritoController = {
-    favorito: async (req,res)=>{
-        const idCurso = req.body.cursoId
-        await Curso.findOne({_id : idCurso})
-        const favorito = Curso.favorito.some(fav => fav.toString() === req.body.cursoId.toString())
-        const action = favorito ? "$pull" : "$push"
-        Curso.findOneAndUpdate(
-            {_id:idCurso},{[action]:{favorito: req.body.cursoId}},
-            {new:true}
-        )
-        .then((response) => {
-            res.json({response})
-        })
-        .catch((err) => console.log(err))
+// const favoritoController = {
+//     favorito: async (req,res)=>{
+//         const idCurso = req.body.cursoId
+//         await Curso.findOne({_id : idCurso})
+//         const favorito = Curso.favorito.some(fav => fav.toString() === req.body.cursoId.toString())
+//         const action = favorito ? "$pull" : "$push"
+//         Curso.findOneAndUpdate(
+//             {_id:idCurso},{[action]:{favorito: req.body.cursoId}},
+//             {new:true}
+//         )
+//         .then((response) => {
+//             res.json({response})
+//         })
+//         .catch((err) => console.log(err))
         
-    }
-}
+//     }
+// }
 
-module.exports = favoritoController 
+// module.exports = favoritoController 
