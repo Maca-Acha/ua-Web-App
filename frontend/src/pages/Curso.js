b88c4b0f270c98597183dd2c24a691f3db11d88d
lucianhoff <lucianoaguirre007@gmail.com>
2021-12-26 17:27:37 -0300

    cositas

--- frontend/src/pages/Curso.js
+++ frontend/src/pages/Curso.js
@@ -1,22 +1,27 @@
 import React from "react";
 
 const Curso = () => {
+
+  // 
+
+
   return (
     <>
-      {/* <iframe
-      className="w-96 mt-32 h-96"
-        // width="560"
-        // height="315"
-        src="https://www.youtube.com/embed/ng-afKOz2eM"
-        title="Clase"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe> */}
-
-      <div class="container mt-32 bg-gray-400 min-h-screen rounded-md flex items-center">
-        
+      <div class="container mt-20 bg-gray-400 vh-70 flex items-center">
+        <iframe
+          className="h-full w-full"
+          // width="560"
+          // height="315"
+          src="https://www.youtube.com/embed/ng-afKOz2eM"
+          title="Clase"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
       </div>
+
+
+    
     </>
   );
 };
