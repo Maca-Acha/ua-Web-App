f20a8bc04068e5eeac12eb661b4d336db1abedf7
lucianhoff <lucianoaguirre007@gmail.com>
2021-12-27 00:00:36 -0300

    logueo

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,66 +1,112 @@
-const bcryptjs = require ('bcryptjs')
-const Usuario = require ('../models/Usuario')
-const jwt = require('jsonwebtoken')
+const bcryptjs = require("bcryptjs");
+const Usuario = require("../models/Usuario");
+const jwt = require("jsonwebtoken");
 
 const usuarioControlador = {
-    nuevoUsuario: async(req,res) => {
-        const {nombre, apellido, email, contraseña, foto, google, tutor, admin, role} = req.body
-        try{
-            const usuarioExiste = await Usuario.findOne({email})
-            if(usuarioExiste){
-                res.json({success: false, error:"El email ya esta en uso", response:null})
-            }else{
-                const contraseñaHasheada = bcryptjs.hashSync(contraseña, 10)
-                const nuevoUsuario = new Usuario({
-                    nombre, apellido, email, contraseña: contraseñaHasheada, foto, google, tutor, admin, role
-                })
-                const token = jwt.sign({...nuevoUsuario}, process.env.SECRET_KEY)
-                await nuevoUsuario.save()
-                res.json({success:true, response:{token, ...nuevoUsuario._doc}, error:null})
-            }
-        }catch(e){
-            res.json({success:false,})
-        }
-    },
-    usuariosRegistrados:(req,res) => {
-        Usuario.find().then((response)=>{
-            res.json({response})
-        })
-    },
-    inicioSesion:async(req,res) => {
-        const {contraseña, email} = req.body
-        try{
-            const emailExiste = await Usuario.findOne({email})
-            if(emailExiste){
-                let contraseñaCorrecta = bcryptjs.compareSync(contraseña, emailExiste.contraseña)
-                if(contraseñaCorrecta){
-                    const token = jwt.sign({...emailExiste}, process.env.SECRET_KEY)
-                    res.json({success:true, response:{token, emailExiste }, error:null})
-                }else{
-                    res.json({success: false, error: "La contraseña es incorrecta", response: null})
-                }
-            }else{
-                res.json({success: false, error: "El email es incorrecto", response: null})
-            }
-        }catch(error){
-        res.json({success: false, response: null, error: error})
-        }
-    },
-    chekearToken: (req, res) => {
-        res.json(req.usuario)
-    },
-    obtenerRoles: async (req, res) => {
-        
-        try{
-            if(req,usuario){
-                res.json({success: true, response:res.usuario, error:null})
-            }
-
-        }catch(error){
-            res.json({success:false, response:null, error:error})
-        }
+  nuevoUsuario: async (req, res) => {
+    const {
+      nombre,
+      apellido,
+      email,
+      contraseña,
+      foto,
+      google,
+      tutor,
+      admin,
+      role,
+    } = req.body;
+    try {
+      const usuarioExiste = await Usuario.findOne({ email });
+      if (usuarioExiste) {
+        res.json({
+          success: false,
+          error: "El email ya esta en uso",
+          response: null,
+        });
+      } else {
+        const contraseñaHasheada = bcryptjs.hashSync(contraseña, 10);
+        const nuevoUsuario = new Usuario({
+          nombre,
+          apellido,
+          email,
+          contraseña: contraseñaHasheada,
+          foto,
+          google,
+          tutor,
+          admin,
+          role,
+        });
+        const token = jwt.sign({ ...nuevoUsuario }, process.env.SECRET_KEY);
+        await nuevoUsuario.save();
+        res.json({
+          success: true,
+          response: { token, ...nuevoUsuario._doc },
+          error: null,
+        });
+      }
+    } catch (e) {
+      res.json({ success: false });
     }
-   
-}
+  },
+  usuariosRegistrados: (req, res) => {
+    Usuario.find().then((response) => {
+      res.json({ response });
+    });
+  },
+  inicioSesion: async (req, res) => {
+    const { email, contraseña, google} = req.body;
 
-module.exports = usuarioControlador
+    try {
+      const emailExiste = await Usuario.findOne({ email });
+      console.log(emailExiste);
+
+      if (emailExiste) {
+        let contraseñaCorrecta = bcryptjs.compareSync(
+          contraseña,
+          emailExiste.contraseña
+        );
+        console.log(contraseñaCorrecta);
+        if (contraseñaCorrecta) {
+          const token = jwt.sign({ ...emailExiste }, process.env.SECRET_KEY);
+          console.log(token);
+          res.json({
+            success: true,
+            response: { token, ...emailExiste._doc },
+            error: null
+          });
+        } else {
+          res.json({
+            success: false,
+            error: "La contraseña es incorrecta",
+            response: null,
+          });
+        }
+      } else {
+        console.log(emailExiste);
+        res.json({
+          success: false,
+          error: "El email es incorrecto",
+          response: null,
+        });
+      }
+    } catch (error) {
+      res.json({ success: false, response: null, error: error });
+    }
+  },
+  chekearToken: (req, res) => {
+    res.json(req.usuario);
+  },
+  obtenerRoles: async (req, res) => {
+    try {
+      // console.log("en el controller")
+      // console.log(req.usuario);
+      if ((req.usuario)) {
+        res.json({ success: true, response: res.usuario, error: null });
+      }
+    } catch (error) {
+      res.json({ success: false, response: null, error: error });
+    }
+  },
+};
+
+module.exports = usuarioControlador;
--- frontend/src/redux/actions/usuarioAction.js
+++ frontend/src/redux/actions/usuarioAction.js
@@ -1,36 +1,45 @@
-import axios from "axios"
+import axios from "axios";
 
 const usuarioAction = {
+  obtenerRoles: (token) => {
+    return async (dispatch, getState) => {
+      try {
+        const respuesta = await axios.post(
+          "http://localhost:4000/api/registrarse",
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log(respuesta);
+        dispatch({ type: "ROLES", payload: { rol: respuesta.data.rol } });
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+  },
 
-    obtenerRoles:(token) => {
-        return async (dispatch,getState) => {
-            try{
-                const respuesta = await axios.post("http://localhost:4000/api/registrarse",{},{
-                   headers: {
-                       Authorization:  "Bearer "  + token 
-                   }
-                })
-                console.log(respuesta)
-                dispatch({type:"ROLES",payload:{rol:respuesta.data.rol}})  
-            }catch(e){
-                console.log(e.message)
-            }
+  inicioSesion: (values) => {
+    return async (dispatch, getState) => {
+      try {
+        const usuario = await axios.post(
+          "http://localhost:4000/api/inicioSesion",
+          { ...values }
+        );
+        console.log(usuario);
+        if (usuario.data.success && !usuario.data.error) {
+          localStorage.setItem("token", usuario.data.response.token);
+          dispatch({ type: "INICIO_SESION", payload: usuario.data });
+          return { success: true, response: usuario.data };
+        } else {
+          return { error: usuario.data.error };
         }
-    },
-
-    inicioSesion:async (usuario) =>{
-        try{
-            const respuesta = await axios.post("http://localhost:4000/api/inicioSesion",{...usuario})
-            if(respuesta.data.success && !respuesta.data.error){
-                localStorage.setItem("token", respuesta.data.response.token)
-            }else{
-                return {error: [respuesta.data.error]}
-                
-            }
-
-        }catch(e){
-            console.log(e.message)
-        }
-    }
-}
-export default usuarioAction
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+  },
+};
+export default usuarioAction;
