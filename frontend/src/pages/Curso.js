ee25228786212a566633d3496c0d6914e2783290
lucianhoff <lucianoaguirre007@gmail.com>
2021-12-27 13:20:19 -0300

    colores

--- frontend/src/pages/Curso.js
+++ frontend/src/pages/Curso.js
@@ -1,12 +1,39 @@
 import React, { useState } from "react";
 
 const Curso = () => {
-  //
-
-  const [mostrarDescripcion, setMostrarDescripcion] = useState(false);
+  const [mostrarDescripcion, setMostrarDescripcion] = useState(true);
   const [mostrarOpiniones, setMostrarOpiniones] = useState(false);
   const [mostrarNotas, setMostrarNotas] = useState(false);
   const [mostrarRecursos, setMostrarRecursos] = useState(false);
+
+  const colores = (x) => {
+    if (x === "1") {
+      setMostrarDescripcion(true);
+      setMostrarOpiniones(false);
+      setMostrarNotas(false);
+      setMostrarRecursos(false);
+    } else if (x === "2") {
+      setMostrarDescripcion(false);
+      setMostrarOpiniones(true);
+      setMostrarNotas(false);
+      setMostrarRecursos(false);
+    } else if (x === "3") {
+      setMostrarDescripcion(false);
+      setMostrarOpiniones(false);
+      setMostrarNotas(true);
+      setMostrarRecursos(false);
+    } else if (x === "4") {
+      setMostrarDescripcion(false);
+      setMostrarOpiniones(false);
+      setMostrarNotas(false);
+      setMostrarRecursos(true);
+    } else {
+      setMostrarDescripcion(false);
+      setMostrarOpiniones(false);
+      setMostrarNotas(false);
+      setMostrarRecursos(false);
+    }
+  };
 
   return (
     <>
@@ -30,16 +57,11 @@ const Curso = () => {
         </div>
       </div>
 
-      <div className="bg-red-500 w-full h-20 flex ">
+      <div className="bg-purple-500 w-full h-20 flex flex-col md:flex-row ">
         <div className="flex w-full md:w-6/12 flex-col md:flex-row">
           <div
-            className="bg-blue-500 flex items-center justify-center  md:w-6/12 h-full"
-            onClick={() => {
-              setMostrarDescripcion(!mostrarDescripcion);
-              setMostrarOpiniones(false);
-              setMostrarNotas(false);
-              setMostrarRecursos(false);
-            }}
+            className="bg-blue-500 flex items-center justify-center py-5 md:py-0 md:w-6/12 h-full"
+            onClick={() => colores("1")}
           >
             <h2 className="font-bold text-xl md:text-2xl lg:text-3xl text-center font-heading text-white cursor-pointer">
               Descripción general
@@ -48,12 +70,7 @@ const Curso = () => {
 
           <div
             className="bg-green-500 md:w-6/12 h-full flex items-center justify-center cursor-pointer"
-            onClick={() => {
-              setMostrarDescripcion(false);
-              setMostrarOpiniones(!mostrarOpiniones);
-              setMostrarNotas(false);
-              setMostrarRecursos(false);
-            }}
+            onClick={() => colores("2")}
           >
             <h2 className="font-bold text-center text-xl md:text-2xl lg:text-3xl font-heading text-white">
               Opiniones
@@ -63,26 +80,17 @@ const Curso = () => {
 
         <div className="flex w-full md:w-6/12 flex-col md:flex-row">
           <div
-            className="bg-red-500 flex items-center justify-center  md:w-6/12 h-full"
-            
-            onClick={() => {
-              setMostrarDescripcion(false);
-              setMostrarOpiniones(false);
-              setMostrarNotas(true);
-              setMostrarRecursos(false);
-            }}
+            className="bg-purple-500 flex items-center justify-center  md:w-6/12 h-full cursor-pointer"
+            onClick={() => colores("3")}
           >
-            <h2 className="font-bold text-xl md:text-2xl lg:text-3xl font-heading text-white">Notas</h2>
+            <h2 className="font-bold text-xl md:text-2xl lg:text-3xl font-heading text-white">
+              Notas
+            </h2>
           </div>
 
           <div
-            className="bg-purple-500 md:w-6/12 h-full flex items-center justify-center"
-            onClick={() => {
-              setMostrarDescripcion(true);
-              setMostrarOpiniones(false);
-              setMostrarNotas(false);
-              setMostrarRecursos(true);
-            }}
+            className="bg-purple-500 md:w-6/12 h-full flex items-center justify-center cursor-pointer"
+            onClick={() => colores("4")}
           >
             <h2 className="font-bold text-center text-xl md:text-2xl lg:text-3xl font-heading text-white">
               Recursos
@@ -90,7 +98,8 @@ const Curso = () => {
           </div>
         </div>
       </div>
-      {!mostrarDescripcion && (
+
+      {mostrarDescripcion && (
         <div className="h-full vh-70 w-full bg-blue-500">
           <span className="invisible"> a </span>
         </div>
@@ -103,7 +112,7 @@ const Curso = () => {
       )}
 
       {mostrarNotas && (
-        <div className="h-full vh-70 w-full bg-red-500">
+        <div className="h-full vh-70 w-full bg-purple-500">
           <span className="invisible"> a </span>
         </div>
       )}
