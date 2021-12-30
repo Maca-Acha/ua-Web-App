4eaee549eda893bdecf9c6c3ae0ff26a74254b25
lucianhoff <lucianoaguirre007@gmail.com>
2021-12-30 06:23:30 -0300

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,29 @@
+require("dotenv").config();
 
-require('dotenv').config()
+const Router = require("./routes/routes");
+const passport = require("passport");
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-const Router = require('./routes/routes')
-const passport = require('passport')
-const express = require("express")
-const cors = require("cors")
-const app = express()
-require('./config/database')
+require("./config/database");
+const path = require("path");
 
-app.use(cors())
-app.use(express.json())
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+app.use(cors());
+app.use(express.json());
 app.use(passport.initialize());
-app.use('/api', Router)
+app.use("/api", Router);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+}
 
-app.listen(4000,() => {console.log("Server listening on port 4000")})
+app.listen(process.env.PORT || "4000", () => {
+  console.log(`El server esta en el puerto ${process.env.PORT || "5000"}`);
+});
